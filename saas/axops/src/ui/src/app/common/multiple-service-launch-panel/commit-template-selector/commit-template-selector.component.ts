b9b64167942c6d392456856c3ef8132f0bff9168
wokeGit <wkalemba.net@gmail.com>
2017-10-16 16:24:01 +0200

    #264 Added table styles, table header for browse. Style adjustment.

--- saas/axops/src/ui/src/app/common/multiple-service-launch-panel/commit-template-selector/commit-template-selector.component.ts
+++ saas/axops/src/ui/src/app/common/multiple-service-launch-panel/commit-template-selector/commit-template-selector.component.ts
@@ -1,5 +1,10 @@
+import * as _ from 'lodash';
 import { Component, OnInit } from '@angular/core';
 import { RepoService } from '../../../services';
+
+import { Template } from '../../../model';
+
+type SelectorParts = 'repositories' | 'branches' | 'commits' | 'templates';
 
 @Component({
     selector: 'ax-commit-template-selector',
@@ -7,23 +12,96 @@ import { RepoService } from '../../../services';
     styles: [ require('./commit-template-selector.scss') ],
 })
 export class CommitTemplateSelectorComponent implements OnInit {
-    public qwe: any  = {
-        repositories: [],
-        branches: [],
-        commits: [],
-        templates: []
+    public selectorSteps: any  = {
+        repositories: {
+            isActive: true,
+            active: {
+                name: '',
+                url: '',
+            },
+            messages: {
+                title: 'Select a repo',
+                searchInputPlaceholder: 'Search repo'
+            },
+            items: []
+        },
+        branches: {
+            isActive: false,
+            active: {
+                name: '',
+                url: '',
+            },
+            messages: {
+                title: 'Select a branch',
+                searchInputPlaceholder: 'Search branch'
+            },
+            items: []
+        },
+        commits: {
+            isActive: false,
+            active: {
+                name: '',
+                url: '',
+            },
+            messages: {
+                title: 'Select a commit',
+                searchInputPlaceholder: 'Search commit'
+            },
+            items: []
+        },
+        templates: {
+            isActive: false,
+            active: {
+                name: '',
+                url: '',
+            },
+            messages: {
+                title: 'Select service template',
+                searchInputPlaceholder: 'Search template'
+            },
+            items: []
+        }
     };
+    public activePart: SelectorParts = 'repositories';
 
     constructor(private repoService: RepoService) {
     }
 
     ngOnInit() {
-        this.repoService.getReposAsync().toPromise().then(repos => {
-            console.log('repos', repos);
+        this.repoService.getReposAsync().toPromise().then(res => {
+            let repositories = _.map(res.data, (repo: string) => {
+                let repoSplit = repo.split('/');
+
+                return {
+                    name: repoSplit[repoSplit.length - 1],
+                    url: repo
+                };
+            }).sort((a, b) => {
+                if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                    return -1;
+                }
+                if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            this.selectorSteps.repositories.items = repositories;
+            console.log('repos', res, repositories);
         });
     }
 
     public onBrowse() {
 
+    }
+
+    public setActiveStep(stepName: SelectorParts) {
+        this.activePart = stepName;
+        this.selectorSteps.repositories.isActive = false;
+        this.selectorSteps.branches.isActive = false;
+        this.selectorSteps.commits.isActive = false;
+        this.selectorSteps.templates.isActive = false;
+
+        this.selectorSteps[stepName].isActive = true;
     }
 }
