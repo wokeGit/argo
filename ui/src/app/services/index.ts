776bc80e3108c5d6ab7ec499d1e18fa2a01dd644
wokeGit <wkalemba.net@gmail.com>
2017-11-17 12:15:58 +0100

    Added missing services

--- ui/src/app/services/index.ts
+++ ui/src/app/services/index.ts
@@ -3,3 +3,5 @@ export { FeaturesSetsAccessControl, FeaturesSetsService } from './features-sets.
 export { GlobalSearchService } from './global-search.service';
 export { BranchService } from './branch.service';
 export { RepoService } from './repo.service';
+export { ToolService } from './tool.service';
+export { ViewPreferencesService } from './view-preferences.service';
