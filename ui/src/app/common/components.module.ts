7061d4729a98708c683bf19e3844d24a9f5bbe2e
wokeGit <wkalemba.net@gmail.com>
2017-11-28 16:07:02 +0100

    Added ButtonWaveDirective

--- ui/src/app/common/components.module.ts
+++ ui/src/app/common/components.module.ts
@@ -7,8 +7,10 @@ import { WorkflowStatusPipe } from './workflow-status.pipe';
 import { LoaderListMockupComponent } from './loader-list-mockup/loader-list-mockup.component';
 import { ModalComponent } from './modal/modal.component';
 import { ModalTemplateComponent } from './modal/modal.template';
+import { ButtonWaveDirective } from './directives/button-wave/button-wave.directive';
 
 const components = [
+  ButtonWaveDirective,
   DurationPipe,
   TimestampPipe,
   WorkflowStatusPipe,
